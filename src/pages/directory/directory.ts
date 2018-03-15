6db4775a98037c8593cd0cf0800a307766be95ea
rohandangsharma <rohandangsharma@gmail.com>
2018-03-15 10:41:33 -0400

    Schedule and Directory Pages

--- src/pages/directory/directory.ts
+++ src/pages/directory/directory.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-directory',
+  templateUrl: 'directory.html'
 })
-export class HomePage {
-
+export class DirectoryPage {
+  
   constructor(public navCtrl: NavController) {
 
   }
